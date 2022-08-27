5f2dc0edbbc05cc7e67862ef9543941d26737943
Mahmoud Tarek <mahmoud.acm.123@gmail.com>
2022-08-27 13:31:33 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 //middlewars
 import middlewares from './middlewares';
@@ -9,10 +11,17 @@ import routes from './routes';
 const app = express();
 const PORT = parseInt(process.env.PORT!, 10) || 3000;
 
-//middlewars
-app.use(...middlewares);
-app.use(...routes);
-
-app.listen(PORT, () => {
-    console.log(`server is running on host ${PORT}`);
+mongoose.connect(process.env.DB_URL!).then(init).catch((error) => {
+    console.log('somthing went wrong with mongodb.\n', JSON.stringify(error));
 });
+
+function init() {
+    console.log('connected to the database.');
+
+    app.use(...middlewares);
+    app.use(...routes);
+
+    app.listen(PORT, () => {
+        console.log(`server is running on host ${PORT}.`);
+    });
+}
